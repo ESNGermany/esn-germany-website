cae0088a16788174998e855d2c8f32a7b0263ed0
kciy <immernochnichtwach@hab-verschlafen.de>
2020-10-19 21:31:09 +0200

    add ESN colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    colors: {
+      "esn-green": "#7ac143",
+      "esn-blue": "#00aeef",
+      "esn-pink": "#ec008c",
+      "esn-orange": "#f47b20",
+      "esn-dark-blue": '#2e3192',
+      black: "#000",
+      white: '#fff',
+    }
   },
   variants: {},
   plugins: [],
