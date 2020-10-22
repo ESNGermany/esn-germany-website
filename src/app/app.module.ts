c99e174f13d6fdf0a237bc2a024a65ef143aa1f4
kciy <immernochnichtwach@hab-verschlafen.de>
2020-10-22 11:39:15 +0200

    add flexlayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   theme: {
+    extend: {},
+    fontFamily: {
+      display: ["Oswald", "sans-serif"],
+      body: ["Lato", "sans-serif"],
+    },
     colors: {
       "esn-green": "#7ac143",
       "esn-blue": "#00aeef",
       "esn-pink": "#ec008c",
       "esn-orange": "#f47b20",
-      "esn-dark-blue": '#2e3192',
+      "esn-dark-blue": "#2e3192",
       black: "#000",
-      white: '#fff',
-    }
+      white: "#fff",
+      footer: "#363c3b",
+      "white-bg": "#ebebeb",
+    },
+    maxWidth: {
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+    },
   },
   variants: {},
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
