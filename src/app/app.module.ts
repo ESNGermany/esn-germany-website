eb88929379e6f05c7a4aa5ac77d451e4ad0ac0eb
kciy <immernochnichtwach@hab-verschlafen.de>
2020-10-26 15:54:31 +0100

    move pages and components to folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-// import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent /**LandingPageComponent**/],
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    NavigationComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
