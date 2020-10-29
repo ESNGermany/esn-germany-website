88e80b114416c493496006a186cc85d993734f36
kciy <immernochnichtwach@hab-verschlafen.de>
2020-10-29 16:28:03 +0100

    add partners and students pages

---
+++ src/app/pages/partners-page/partners-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partners-page',
+  templateUrl: './partners-page.component.html',
+  styleUrls: ['./partners-page.component.scss'],
+})
+export class PartnersPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
