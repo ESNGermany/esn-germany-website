ebaa5458c9c40b2cb5e00beabc19e1caf7580034
kciy <immernochnichtwach@hab-verschlafen.de>
2020-10-31 11:46:53 +0100

    add carousel for partners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavigationDarkComponent } from './components/navigation-dark/navigation
 import { FooterComponent } from './components/footer/footer.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { MatDividerModule } from '@angular/material/divider';
     FlexLayoutModule,
     MatIconModule,
     MatDividerModule,
+    IvyCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
