778580b4fb2580d6fd01609fd6c499791d43b333
kciy <immernochnichtwach@hab-verschlafen.de>
2020-11-01 19:14:51 +0100

    add news service

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+interface NewsItem {
+  id: string;
+  Title: string;
+  Shorttext: string;
+  Image: {
+    id: string;
+    alternativeText: string;
+    caption: string;
+    width: number;
+    height: number;
+    formats: {
+      medium: {
+        url: string;
+      };
+    };
+  };
+  Author: string;
+  Text: string;
+  published_at: Date;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  error = null;
+  public newsItemList: NewsItem[];
+  public newsItemListChanged = new Subject<NewsItem[]>();
+
+  constructor(private http: HttpClient) {}
+
+  fetchNewsList(): void {
+    const url = 'https://strapi.esn-germany.de/news-items?_limit=5';
+    this.http.get<any>(url).subscribe((data) => {
+      data.map((item) => {
+        this.newsItemList.push(item);
+      });
+      this.newsItemListChanged.next(this.newsItemList);
+    });
+  }
+}
