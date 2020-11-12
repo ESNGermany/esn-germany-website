89108d4e65d36bc828b9393df3d56ee21b21e27e
kciy <immernochnichtwach@hab-verschlafen.de>
2020-11-12 10:24:33 +0100

    add gridlist

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  imports: [CommonModule, MatDividerModule, MatExpansionModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+  ],
   providers: [],
-  exports: [MatDividerModule, MatExpansionModule, MatIconModule],
+  exports: [
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+  ],
   declarations: [],
   entryComponents: [],
 })
