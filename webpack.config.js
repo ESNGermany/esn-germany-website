f88cd811c9a1da15336333ebd3fdf96708c6c18b
Lukas Heddedndorp <lu.heddendorp@gmail.com>
2020-12-04 11:49:58 +0100

    Optimize images on the landing page

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,57 @@
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminWebp = require("imagemin-webp");
+const glob = require("glob");
 module.exports = {
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
           postcssOptions: {
-            ident: 'postcss',
-            syntax: 'postcss-scss',
+            ident: "postcss",
+            syntax: "postcss-scss",
             plugins: [
-              require('postcss-import'),
-              require('tailwindcss'),
-              require('autoprefixer'),
+              require("postcss-import"),
+              require("tailwindcss"),
+              require("autoprefixer"),
             ],
           },
         },
       },
     ],
   },
+  plugins: [
+    new ImageminPlugin({
+      disable: process.env.IMAGEMIN !== "enabled", // Disable during development
+      // jpegtran: { progressive: false },
+      // optipng: { optimizationLevel: 3 },
+      // svgo: {},
+      // pngquant: {
+      //   quality: "95-100",
+      // },
+      externalImages: {
+        context: "src",
+        destination: "dist/esn-germany-website/browser",
+        fileName: "[path][name].webp",
+        sources: glob.sync("src/assets/**/*.{jpg,png}"),
+      },
+      plugins: [imageminWebp()],
+    }),
+    // new ImageminPlugin({
+    //   // disable: process.env.NODE_ENV !== "production", // Disable during development
+    //   // jpegtran: { progressive: false },
+    //   // optipng: { optimizationLevel: 3 },
+    //   // svgo: {},
+    //   // pngquant: {
+    //   //   quality: "95-100",
+    //   // },
+    //   // externalImages: {
+    //   //   context: "src",
+    //   //   // fileName: "[path][name]-optim.[ext]",
+    //   //   destination: "dist/esn-germany-website/browser",
+    //   //   sources: glob.sync("src/assets/**/*.{jpg,png,svg}"),
+    //   // },
+    // }),
+  ],
 };
