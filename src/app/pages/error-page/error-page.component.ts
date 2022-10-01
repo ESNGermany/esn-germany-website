2967007ce79726d88d1aba36781ceadc8c63bd2a
kciy <immernochnichtwach@hab-verschlafen.de>
2022-10-01 21:41:27 +0200

    use esn-article in error page

--- src/app/pages/error-page/error-page.component.ts
+++ src/app/pages/error-page/error-page.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './error-page.component.html',
   styleUrls: ['./../../../styles.scss'],
 })
-export class ErrorPageComponent {
-  constructor() {}
-}
+export class ErrorPageComponent {}
