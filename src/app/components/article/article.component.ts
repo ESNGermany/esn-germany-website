5735643212e6fb6c092d52910120a1aa667c5080
kciy <immernochnichtwach@hab-verschlafen.de>
2022-10-01 21:20:10 +0200

    add esn-articles to landing page

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'esn-article',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./../../../styles.scss']
 })
 export class ArticleComponent{
-  constructor() { }
+  @Input() title!: string;
 }
