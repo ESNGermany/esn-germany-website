07303690ff63813d760c32385c0a3485dc22dbd6
kciy <immernochnichtwach@hab-verschlafen.de>
2022-10-01 20:54:29 +0200

    add article component

---
+++ src/app/components/article/article.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'esn-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./../../../styles.scss']
+})
+export class ArticleComponent{
+  constructor() { }
+}
